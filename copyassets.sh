a85c5a5c06da478e4457dccee57fe3ffd73cb4a9
Pratik Kelwalkar <pratik.kelwalkar@metamagic.in>
2018-03-02 12:44:06 +0530

    added: server

--- copyassets.sh
+++ copyassets.sh
@@ -2,5 +2,6 @@
 cp -a src/assets/ dist/
 echo "Copied assets folder to dist"
 echo "Copy dist/ to Apache Server"
-cd dist && npm pack && cp -a platform-commons-0.0.0.tgz /var/www/html/desireplatform/
+cd dist && npm pack && mv platform-commons-0.0.0.tgz platform-commons.tgz
+cp -a platform-commons.tgz /var/www/html/desireplatform/micro-ui-libs/
 
