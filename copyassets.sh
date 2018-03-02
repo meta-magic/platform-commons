e7fa7bcb5212adb2d9be2512e49c465626fe0fb6
Pratik Kelwalkar <pratik.kelwalkar@metamagic.in>
2018-03-02 11:50:44 +0530

    added: server

---
+++ copyassets.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+cp -a src/assets/ dist/
+echo "Copied assets folder to dist"
+echo "Copy dist/ to Apache Server"
+cd dist && npm pack && cp -a platform-commons-0.0.0.tgz /var/www/html/desireplatform/
+
