ed7c72207fd9bd46d1ea2810424235ee8f18b089
Pratik Kelwalkar <pratik.kelwalkar@metamagic.in>
2018-04-02 18:15:20 +0530

    added: guards

--- src/route-guards/session.token.gaurd.ts
+++ src/route-guards/session.token.gaurd.ts
@@ -2,34 +2,39 @@
  * Created by pratik on 20/2/18.
  */
 import { Injectable } from '@angular/core';
-import {CanActivate} from "@angular/router";
-import {CookieService} from "../cookie-service/cookie.service";
-import {COOKIE_NAME} from "../http-interceptor/platform.http.interceptor";
+import { CanActivate, CanLoad } from '@angular/router';
+import { CookieService } from '../cookie-service/cookie.service';
+import { COOKIE_NAME } from '../http-interceptor/platform.http.interceptor';
 
 @Injectable()
-export class UserAuthenticGuard implements CanActivate{
+export class UserAuthenticGuard implements CanLoad {
+  constructor(private cookieService: CookieService) {}
 
- constructor(private cookieService : CookieService) { }
-
- canActivate(){
+  /*canActivate(){
    if(this.isLoggedIn())
      return true;
    else{
      window.alert('Invalid user ');
      return false;
    }
- }
+ }*/
 
- isLoggedIn() : boolean{
-   let isUserValid : boolean = false;
-   let cookieData = this.cookieService.get(COOKIE_NAME);
-   if(cookieData != ""){
-     // make http call to verify token?
-     isUserValid = true;
-   }
-   else{
-     isUserValid = false;
-   }
-   return isUserValid;
- }
+  canLoad() {
+    if (this.isLoggedIn()) return true;
+    else {
+      window.alert('Invalid user');
+      return false;
+    }
+  }
+
+  isLoggedIn(): boolean {
+    let isUserValid: boolean = false;
+    let cookieData = this.cookieService.get(COOKIE_NAME);
+    if (cookieData != '') {
+      isUserValid = true;
+    } else {
+      isUserValid = false;
+    }
+    return isUserValid;
+  }
 }
