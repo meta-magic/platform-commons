6f3244fce90340750bfcc64418a975f57366cca1
datsgawas <dattaram.gawas@metamagic.in>
2018-08-30 02:19:59 +0530

    removeItem method removed

--- src/local-storage-service/local.storage.service.ts
+++ src/local-storage-service/local.storage.service.ts
@@ -2,37 +2,36 @@
  * Created by pratik on 20/2/18.
  */
 import { Injectable } from '@angular/core';
-import {EncryptionService} from "../encryption-service/encryption.service";
+import { EncryptionService } from '../encryption-service/encryption.service';
 
 @Injectable()
 export class LocalStorageService {
+  constructor(private encryptionService: EncryptionService) {}
 
-  constructor(private encryptionService : EncryptionService) { }
-
-  set(key : any,value : any){
-    if(key != null){
+  set(key: any, value: any) {
+    if (key != null) {
       //Encrypt before storing
-      localStorage.setItem(key,value === undefined ? null : this.encryptionService.encryptObject(value));
-    }
-    else{
+      localStorage.setItem(
+        key,
+        value === undefined ? null : this.encryptionService.encryptObject(value)
+      );
+    } else {
       console.warn('Local Storage Service : Key cannot be empty');
     }
   }
 
-  get(key:any) : any{
-    let data : any;
-    if(key != null){
+  get(key: any): any {
+    let data: any;
+    if (key != null) {
       let stringData = localStorage.getItem(key);
       //remove from local storage
-      localStorage.removeItem(key);
-      if(stringData != null){
+      // localStorage.removeItem(key);
+      if (stringData != null) {
         data = this.encryptionService.decryptObject(stringData);
-      }
-      else{
+      } else {
         console.warn('No Item found in local storage');
       }
-    }
-    else{
+    } else {
       console.warn('Local Storage Service : Key cannot be empty');
     }
     return data;
